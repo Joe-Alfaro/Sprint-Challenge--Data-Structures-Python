368ee462a4603f94dff6d77f2f6e28fb4d5ed26e
joe-alfaro <josephlalfaro@gmail.com>
2019-07-26 09:03:37 -0700

    use dict to store name counts and return duplicates

--- names/names.py
+++ names/names.py
@@ -10,12 +10,26 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+nameCounts = {}
 
+for name in names_1:
+    nameCounts[name] = 1
+
+for name in names_2:
+    if name in nameCounts:
+        nameCounts[name] += 1
+
+duplicates = [name for name in nameCounts if nameCounts[name] > 1] 
+
+
+'''
+    duplicates = []
+    for name_1 in names_1:
+        for name_2 in names_2:
+            if name_1 == name_2:
+                duplicates.append(name_1)
+
+'''
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
